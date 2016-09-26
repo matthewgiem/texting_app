ae29f33c0b0123822defaa471ff8e57bc43ee70d
Dylan Barnard and Matthew Giem <dylan+matthew@>
2016-09-26 11:19:35 -0700

    Use .env file to store authorization info

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,8 +7,8 @@ private
     response = RestClient::Request.new(
       :method => :post,
       :url => 'https://api.twilio.com/2010-04-01/Accounts/AC71a7982ab5d7a322a2cf3607cd05e8e2/Messages.json',
-      :user => 'AC71a7982ab5d7a322a2cf3607cd05e8e2',
-      :password => '4bde80e20747c855c0bd8b04d4fde1f4',
+      :user => ENV['TWILIO_ACCOUNT_SID'],
+      :password => ENV['TWILIO_AUTH_TOKEN'],
       :payload => { :Body => body,
                     :From => from,
                     :To => to }
