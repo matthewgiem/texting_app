505571f0cf75a5c304e415131f8f779a287e9e78
Dylan Barnard and Matthew Giem <dylan+matthew@>
2016-09-27 11:50:22 -0700

    Add bootstrap and update styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => 'messages#new'
+
   resources :messages, :only => [:index, :new, :create, :show]
   resources :contacts, :only => [:new, :create]
 end
