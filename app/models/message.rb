8535a4150f8271bfcc617af421db660c9d77b383
Dylan Barnard and Matthew Giem <dylan+matthew@>
2016-09-27 10:47:02 -0700

    Add get and send cat fact functionality

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,15 +4,23 @@ class Message < ActiveRecord::Base
 
 private
 
+  def get_cat_fact
+    response = RestClient::Request.new(
+    :method => :get,
+    :url => 'http://catfacts-api.appspot.com/api/facts?number=1'
+    ).execute
+  end
+
   def send_message
     begin
-      binding.pry
+      cat_response = JSON.parse(get_cat_fact)
+      message = body + ": " + cat_response["facts"][0]
       response = RestClient::Request.new(
         :method => :post,
         :url => 'https://api.twilio.com/2010-04-01/Accounts/AC71a7982ab5d7a322a2cf3607cd05e8e2/Messages.json',
         :user => ENV['TWILIO_ACCOUNT_SID'],
         :password => ENV['TWILIO_AUTH_TOKEN'],
-        :payload => { :Body => body,
+        :payload => { :Body => message,
                       :From => from,
                       :To => contact.phone_number }
       ).execute
